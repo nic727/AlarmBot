fc4cdaa2fb6752e6df592868d7cb7e89d3d8c2fb
Grzegorz <wilkolakpl@gmail.com>
2018-04-07 21:28:09 +0100

    sin wave

--- alarmCode/alarmCode.ino
+++ alarmCode/alarmCode.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <Time.h>
+#include <TimeLib.h>
+
+
+int screaming = 0;
+
+void setup()
+{
+  pinMode(1, OUTPUT);
+  Serial.begin(115200); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if (screaming){
+    tones(1000);
+  }
+  Serial.println(sin(millis()*PI/1000)/2 + 0.5);
 }
+
+void tones(int delay){
+  analogWrite(1, 2000);
+  delayMicroseconds(sin(millis()*PI/1000)/2 + 0.5);
+  analogWrite(1, 0);
+  delayMicroseconds(sin(millis()*PI/1000)/2 + 0.5);
+}
+
