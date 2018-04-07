e22aac114776b2531df7dbfb8daef65999dd6a06
Nina Caisova <nina.cais@gmail.com>
2018-04-07 21:16:37 +0100

    led random sequence

--- alarmCode/alarmCode.ino
+++ alarmCode/alarmCode.ino
@@ -2,9 +2,6 @@ int redPin=9;  //set red LED pin to 11
 int greenPin=7; //set green LED pin to 10
 int bluePin=8; //set blue LED pin to 6
 
-int redPin2=6;
-int bluePin2=5;
-int greenPin2=4;
 int brightness=100;
 const int sequence_len = 5;
 int sequence[sequence_len];
@@ -12,6 +9,7 @@ bool is_done = false;
 int red_light = 0;
 int blue_light = 0;
 int green_light = 0;
+int count = 0;
   
 
 void setup() {
@@ -19,42 +17,46 @@ void setup() {
 Serial.begin(9600); //Turn on Serial port
 pinMode(redPin, OUTPUT); //Set redPin to be an output
 pinMode(greenPin, OUTPUT); //Set greenPin to be an output
-pinMode(redPin2, OUTPUT); //Set redPin to be an output
-pinMode(greenPin2, OUTPUT); //Set greenPin to be an output
 pinMode(bluePin, OUTPUT); //set bluePin to be an output
-pinMode(bluePin2, OUTPUT); //set bluePin to be an output
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
- // if(!is_done){
- //   generateColor();
- //for (int i=0; i < sequence_len; i++){
-  switch (random(1,5)) {
-    case 1:
-      red_light = brightness;
-      analogWrite(redPin, red_light);
-      break;
-    case 2:
-      blue_light = brightness;
-      analogWrite(bluePin, blue_light);
-      break;
-    case 3:
-      analogWrite(greenPin, green_light);
-      break;
-    case 4:
-      green_light = brightness;
-      red_light = brightness;
-      analogWrite(greenPin, green_light);
-      analogWrite(bluePin, blue_light);
-      
-    default:
-      red_light = 0;
-      blue_light = 0;
-      green_light = 0;
+  if(count == 0){
+    generateSequence();
   }
+
+  if(count < 5) {
+    switch (sequence[count]) {
+      case 1:
+        red_light = brightness;
+        analogWrite(redPin, red_light);
+        break;
+      case 2:
+        blue_light = brightness;
+        analogWrite(bluePin, blue_light);
+        break;
+      case 3:
+        green_light = brightness;
+        analogWrite(greenPin, green_light);
+        break;
+      case 4:
+        green_light = brightness;
+        red_light = brightness;
+        analogWrite(greenPin, green_light);
+        analogWrite(bluePin, blue_light);
+        
+      default:
+        red_light = 0;
+        blue_light = 0;
+        green_light = 0;
+    }
+  }else {
+        analogWrite(greenPin, 100);
+        analogWrite(bluePin, 100);
+        analogWrite(redPin, 100);
+    }
       delay(2000);
       red_light = 0;
       blue_light = 0;
@@ -62,6 +64,7 @@ void loop() {
       analogWrite(greenPin, green_light);
       analogWrite(bluePin, blue_light);
       analogWrite(redPin, red_light);
+      count++;
 }
 void generateSequence() {
   for(int i =0; i<sequence_len;i++) {
